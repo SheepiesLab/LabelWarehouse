2bc068788f4382ee5b02a68287e0280f5169970a
NG Hok Chun <hcngac@gmail.com>
2021-09-09 16:24:16 +0800

    fix

--- labelwarehouse-api/routes/lwobjects.js
+++ labelwarehouse-api/routes/lwobjects.js
@@ -7,9 +7,9 @@ class LabelWarehouseObject{
     constructor(){};
     update(){};
     remove(){};
-    static add();
-    static get();
-    static search();
+    static add(){};
+    static get(){};
+    static search(){};
 }
 
 module.exports =  LabelWarehouseObject;
