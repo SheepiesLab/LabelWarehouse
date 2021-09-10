4d46157e3a59e835d8714c7ac7231811a71ca84b
NG Hok Chun <hcngac@gmail.com>
2021-09-10 14:26:18 +0800

    validation

--- labelwarehouse-api/routes/items.js
+++ labelwarehouse-api/routes/items.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+const itemSchema = require('../schemas/Item.schema.json');
+const itemsGetRequestSchema = require('../schemas/ItemsGetRequest.json');
+const itemPostRequestSchema = require('../schemas/ItemPostRequest.schema.json');
+
+const itemSchemaValidate = ajv.compile(itemSchema);
+const itemsGetRequestSchemaValidate = ajv.compile(itemsGetRequestSchema);
+const itemPostRequestSchemaValidate = ajv.compile(itemPostRequestSchema);
+
 var LabelWarehouseObject = require('./lwobjects')
 
 class Item extends LabelWarehouseObject {
@@ -15,26 +27,50 @@ class Item extends LabelWarehouseObject {
 }
 
 router.get('/', function(req, res, next) {
+  if (!itemsGetRequestSchemaValidate(req.body)){
+    res.status(400).json({
+      "error": "Request JSON invalid",
+      "details": itemsGetRequestSchemaValidate.errors
+    })
+  }
   res.json(req.body);
 });
 
 router.post('/', function(req, res, next) {
+  if (!itemPostRequestSchemaValidate(req.body)){
+    res.status(400).json({
+      "error": "Request JSON invalid",
+      "details": itemPostRequestSchemaValidate.errors
+    })
+  }
   res.json(req.body);
 })
 
 router.get('/:id', function(req, res, next) {
   id = req.params.id;
-  res.json(req.body);
+  res.json({
+    "action": "get",
+    "id": id
+  });
 });
 
 router.put('/:id', function(req, res, next) {
   id = req.params.id;
+  if (!itemPostRequestSchemaValidate(req.body)){
+    res.status(400).json({
+      "error": "Request JSON invalid",
+      "details": itemPostRequestSchemaValidate.errors
+    })
+  }
   res.json(req.body);
 });
 
 router.delete('/:id', function(req, res, next) {
   id = req.params.id;
-  res.json(req.body);
+  res.json({
+    "action": "delete",
+    "id": id
+  });
 });
 
 module.exports = router;
--- labelwarehouse-api/routes/resources.js
+++ labelwarehouse-api/routes/resources.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+const resourceSchema = require('../schemas/Resource.schema.json');
+const resourcesGetRequestSchema = require('../schemas/ResourcesGetRequest.schema.json');
+const resourcePostRequestSchema = require('../schemas/ResourcePostRequest.schema.json')
+
+const resourceSchemaValidate = ajv.compile(resourceSchema);
+const resourcesGetRequestSchemaValidate = ajv.compile(resourcesGetRequestSchema);
+const resourcePostRequestSchemaValidate = ajv.compile(resourcePostRequestSchema);
+
 var LabelWarehouseObject = require('./lwobjects')
 
 class Resource extends LabelWarehouseObject {
@@ -12,26 +24,49 @@ class Resource extends LabelWarehouseObject {
 }
 
 router.get('/', function(req, res, next) {
+  if (!resourcesGetRequestSchemaValidate(req.body)){
+    res.status(400).json({
+      "error": "Request JSON invalid",
+      "details": resourcesGetRequestSchemaValidate.errors
+    })
+  }
   res.json(req.body);
 });
 
 router.post('/', function(req, res, next) {
+  if (!resourcePostRequestSchemaValidate(req.body)){
+    res.status(400).json({
+      "error": "Request JSON invalid",
+      "details": resourcePostRequestSchemaValidate.errors
+    })
+  }
   res.json(req.body);
 })
 
 router.get('/:id', function(req, res, next) {
   id = req.params.id;
-  res.json(req.body);
+  res.json({
+    "action": "get",
+    "id": id
+  });
 });
 
 router.put('/:id', function(req, res, next) {
-  id = req.params.id;
+  if (!resourcePostRequestSchemaValidate(req.body)){
+    res.status(400).json({
+      "error": "Request JSON invalid",
+      "details": resourcePostRequestSchemaValidate.errors
+    })
+  }
   res.json(req.body);
 });
 
 router.delete('/:id', function(req, res, next) {
   id = req.params.id;
-  res.json(req.body);
+  res.json({
+    "action": "delete",
+    "id": id
+  });
 });
 
 module.exports = router;
