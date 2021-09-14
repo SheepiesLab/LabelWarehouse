06d3834dc2c3be06496c51ec6f1f3189b70a872d
NG Hok Chun <hcngac@gmail.com>
2021-09-14 12:45:03 +0800

    env

--- labelwarehouse-api/utils/env.js
+++ labelwarehouse-api/utils/env.js
@@ -1,11 +1,11 @@
 const {cleanEnv, str, num} = require('envalid');
 
 const env = cleanEnv(process.env, {
-  DBHOST: str({default: 'mongodb'}),
-  DBPORT: num({default: 27017}),
-  DBNAME: str({default: 'labelwarehouse'}),
-  DBUSER: str({default: 'labelwarehouse'}),
-  DBPASS: str(),
+  DB_HOST: str({default: 'mongodb'}),
+  DB_PORT: num({default: 27017}),
+  DB_DATABASE: str({default: 'labelwarehouse'}),
+  DB_USER: str({default: 'labelwarehouse'}),
+  DB_PASSWORD: str(),
 });
 
 module.exports = env;
