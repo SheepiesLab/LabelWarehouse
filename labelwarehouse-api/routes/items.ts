53b887cf16ff01dd270979b72b18f8b4604da3dc
NG Hok Chun <hcngac@gmail.com>
2021-09-15 11:15:20 +0800

    YAGNI

--- labelwarehouse-api/routes/items.ts
+++ labelwarehouse-api/routes/items.ts
@@ -10,29 +10,31 @@ import itemPostRequestSchema from '../schemas/ItemPostRequest.schema.json';
 const itemsGetRequestSchemaValidate = ajv.compile(itemsGetRequestSchema);
 const itemPostRequestSchemaValidate = ajv.compile(itemPostRequestSchema);
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   if (!itemsGetRequestSchemaValidate(req.body)) {
     res.status(400).json({
       'error': 'Request JSON invalid',
       'details': itemsGetRequestSchemaValidate.errors,
     });
-  } else {
-    res.json(req.body);
+    return;
   }
+  res.json(req.body);
+
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   if (!itemPostRequestSchemaValidate(req.body)) {
     res.status(400).json({
       'error': 'Request JSON invalid',
       'details': itemPostRequestSchemaValidate.errors,
     });
-  } else {
-    res.json(req.body);
+    return;
   }
+  res.json(req.body);
+
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
   const id = req.params.id;
   res.json({
     'action': 'get',
@@ -40,19 +42,20 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
   const id = req.params.id;
   if (!itemPostRequestSchemaValidate(req.body)) {
     res.status(400).json({
       'error': 'Request JSON invalid',
       'details': itemPostRequestSchemaValidate.errors,
     });
-  } else {
-    res.json(req.body);
+    return;
   }
+  res.json(req.body);
+
 });
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
   const id = req.params.id;
   res.json({
     'action': 'delete',
